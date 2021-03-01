61a04230cc8e7e7579b7dfb8cd63c8517f1d115c
feli03002 <73816572+feli03002@users.noreply.github.com>
2021-03-01 10:43:56 +0100

    Add project files.

---
+++ Sorterings algoritmer/Algoritmer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorterings_algoritmer
+{
+    class Algoritmer
+    {
+        public static void Bubblesort(List<int> list)
+        {
+            int max = list.Count - 1;
+
+            for (int i = 0; i < max; i++)
+            {
+                int nrLeft = max - i;
+
+                for (int j = 0; j < nrLeft; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void Selectionsort(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int minima = list[i];
+                int mindex = i;
+                int temp;
+
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < minima)
+                    { minima = list[j]; mindex = j; }
+                }
+
+                if (list[mindex] != list[i])
+                {
+                    temp = list[i];
+                    list[i] = list[mindex];
+                    list[mindex] = temp;
+                }
+
+            }
+        }
+        public static List<int> Mergesort(List<int> list)
+        {
+            if (list.Count <= 1)
+                return list;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = list.Count / 2;
+
+            for (int i = 0; i < middle; i++)
+                left.Add(list[i]);
+            for (int i = middle; i < list.Count; i++)
+                right.Add(list[i]);
+
+
+            left = Mergesort(left);
+            right = Mergesort(right);
+
+            return Merge(left, right);
+        }
+        static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        left.RemoveAt(0);
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.RemoveAt(0);
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.RemoveAt(0);
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.RemoveAt(0);
+                }
+
+            }
+            return result;
+        }
+        public static void Quicksort(List<int> data, int min, int max)
+        {
+
+            int pivot = data[(min + max) / 2];
+            int minHold = min;
+            int maxHold = max;
+
+            while (minHold < maxHold)
+            {
+
+                while ((data[minHold] < pivot) && (minHold <= maxHold))
+                    minHold++;
+                while ((data[maxHold] > pivot) && (maxHold >= minHold))
+                    maxHold--;
+
+                if (minHold < maxHold)
+                {
+                    int temp = data[minHold];
+                    data[minHold] = data[maxHold];
+                    data[maxHold] = temp;
+
+                    if (data[minHold] == pivot && data[maxHold] == pivot)
+                        minHold++;
+                }
+            }
+
+            if (min < minHold - 1)
+                Quicksort(data, min, minHold - 1);
+            if (max > maxHold + 1)
+                Quicksort(data, maxHold + 1, max);
+        }
+    }
+}
---
+++ Sorterings algoritmer/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Sorterings_algoritmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tallista = new List<int>();
+            List<string> data = new List<string>();
+            Stopwatch stopwatch = new Stopwatch();
+            int mängd = 1;
+            int försök = 1;
+            int medel = 0;
+            try
+            {
+                 mängd = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException) { }
+
+            try
+            {
+                 försök = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException) { }
+            
+
+            data.Add("Bs");
+            Console.WriteLine("Sorteringsalgoritm: Bubblesort");
+            for(int i = 0; i < mängd; i++)
+            {
+                
+                int ant = Convert.ToInt32(1000 * Math.Pow(2, i));
+                data.Add(Convert.ToString(ant));
+                for (int j = 0; j < försök; j++)
+                {
+                    SkapaLista(tallista, ant);
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                    //Algoritmer.Bubblesort(tallista);
+                    stopwatch.Stop();
+                    Console.WriteLine("Längd på lista: " + ant + " Tid i millisekunder: " + stopwatch.ElapsedMilliseconds);
+                    data.Add(" " + stopwatch.ElapsedMilliseconds + "ms");
+                    medel += Convert.ToInt32(stopwatch.ElapsedMilliseconds);
+                    stopwatch.Reset();
+                    tallista.Clear();
+                }
+                medel /= försök;
+                data.Add(" Medel: " + medel + "ms");
+                medel = 0;
+            }
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorteringsalgoritm: Selectsort");
+            data.Add("Ss");
+            for (int i = 0; i < mängd; i++)
+            {
+                int ant = Convert.ToInt32(1000 * Math.Pow(2, i));
+                data.Add(Convert.ToString(ant));
+                for (int j = 0; j < försök; j++)
+                {
+                    SkapaLista(tallista, ant);
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                   // Algoritmer.Selectionsort(tallista);
+                    stopwatch.Stop();
+                    Console.WriteLine("Längd på lista: " + ant + " Tid i millisekunder: " + stopwatch.ElapsedMilliseconds);
+                    data.Add(" " + stopwatch.ElapsedMilliseconds + "ms");
+                    medel += Convert.ToInt32(stopwatch.ElapsedMilliseconds);
+                    stopwatch.Reset();
+                    tallista.Clear();
+                }
+                medel /= försök;
+                data.Add(" Medel: " + medel + "ms");
+                medel = 0;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorteringsalgoritm: Mergesort");
+            data.Add("Ms");
+            for (int i = 0; i < mängd; i++)
+            {
+                int ant = Convert.ToInt32(1000 * Math.Pow(2, i));
+                data.Add(Convert.ToString(ant));
+                for (int j = 0; j < försök; j++)
+                {
+                    SkapaLista(tallista, ant);
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                   // Algoritmer.Mergesort(tallista);
+                    stopwatch.Stop();
+                    Console.WriteLine("Längd på lista: " + ant + " Tid i millisekunder: " + stopwatch.ElapsedMilliseconds);
+                    data.Add(" " + stopwatch.ElapsedMilliseconds + "ms");
+                    medel += Convert.ToInt32(stopwatch.ElapsedMilliseconds);
+                    stopwatch.Reset();
+                    tallista.Clear();
+                }
+                medel /= försök;
+                data.Add(" Medel: " + medel + "ms");
+                medel = 0;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sorteringsalgoritm: Quicksort");
+            data.Add("Qs");
+            for (int i = 0; i < mängd; i++)
+            {
+                int ant = Convert.ToInt32(1000 * Math.Pow(2, i));
+                data.Add(Convert.ToString(ant));
+                for (int j = 0; j < försök; j++)
+                {
+                    SkapaLista(tallista, ant);
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                    Algoritmer.Quicksort(tallista, 0, tallista.Count - 1);
+                    stopwatch.Stop();
+                    Console.WriteLine("Längd på lista: " + ant + " Tid i millisekunder: " + stopwatch.ElapsedMilliseconds);
+                    data.Add(" " + stopwatch.ElapsedMilliseconds + "ms");
+                    medel += Convert.ToInt32(stopwatch.ElapsedMilliseconds);
+                    stopwatch.Reset();
+                    tallista.Clear();
+                }
+                medel /= försök;
+                data.Add(" Medel: " + medel + "ms");
+                medel = 0;
+            }
+            System.IO.File.WriteAllLines("..\\..\\Data.txt", data);
+            Console.Write("Press to Exit");
+            Console.ReadLine();
+        }
+       
+
+        static void SkapaLista(List<int> list, int ant)
+        {
+            Random r = new Random();
+
+            for(int i = 0; i < ant; i++)
+            {
+                list.Add(r.Next(10000));
+            }
+        }
+       
+        
+    }
+}
